d9937768479d9171ae2344c9e29805eb41cec8df
dev-tristan <jt.jeruta@gmail.com>
2022-06-12 19:21:38 +0800

    Fix lint errors

--- frontend/components/LoginPage/LoginPage.tsx
+++ frontend/components/LoginPage/LoginPage.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import Image from 'next/image'
 import Button from './Button'
 import Input from './Input'
 
@@ -8,12 +9,15 @@ const Login = () => {
         <div className="h-screen flex justify-center items-center">
             <div className="bg-white rounded-lg w-full max-w-sm py-16 px-5">
                 <form>
-                    <div className="flex font-bold justify-center">
-                        <img
-                            className="h-20 w-20"
-                            src="https://raw.githubusercontent.com/sefyudem/Responsive-Login-Form/master/img/avatar.svg"
-                            alt="avatar"
-                        />
+                    <div className="flex justify-center">
+                        <div className="h-24 w-24">
+                            <Image
+                                src="/default-avatar.svg"
+                                alt="avatar"
+                                height="100%"
+                                width="100%"
+                            />
+                        </div>
                     </div>
                     <h2 className="text-3xl text-center text-gray-700 mb-4">
                         Login Form
