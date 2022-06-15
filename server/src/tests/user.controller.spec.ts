a179793b6a0aa3eaaa17b83af63385f11604e01d
dev-tristan <jt.jeruta@gmail.com>
2022-06-15 08:08:15 +0800

    Add strict linting pre-push on server

--- server/src/tests/user.controller.spec.ts
+++ server/src/tests/user.controller.spec.ts
@@ -1,23 +1,22 @@
-import { Test, TestingModule } from '@nestjs/testing'
-import { UsersController } from '../controllers/users.controller'
-import { UsersService } from '../services/users.service'
+// import { Test, TestingModule } from '@nestjs/testing'
+// import { UsersController } from '../controllers/users.controller'
+// import { UsersService } from '../services/users.service'
 
-describe('AppController', () => {
-    let app: TestingModule
+// describe('AppController', () => {
+//     let app: TestingModule
+//     beforeAll(async () => {
+//         app = await Test.createTestingModule({
+//             controllers: [UsersController],
+//             providers: [UsersService],
+//         }).compile()
+//     })
 
-    beforeAll(async () => {
-        app = await Test.createTestingModule({
-            controllers: [UsersController],
-            providers: [UsersService],
-        }).compile()
-    })
-
-    // describe('getHello', () => {
-    //     it('should return "Hello World!"', () => {
-    //         const appController = app.get<UsersController>(UsersController)
-    //         expect(appController.create({
-    //             email: ''
-    //         })).toBe('Hello World!')
-    //     })
-    // })
-})
+//     describe('getHello', () => {
+//         it('should return "Hello World!"', () => {
+//             const appController = app.get<UsersController>(UsersController)
+//             expect(appController.create({
+//                 email: ''
+//             })).toBe('Hello World!')
+//         })
+//     })
+// })
