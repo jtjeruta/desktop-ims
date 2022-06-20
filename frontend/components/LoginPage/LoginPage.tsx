0b2b271b469074dc37423fa76d22c422f9c36ffb
dev-tristan <jt.jeruta@gmail.com>
2022-06-20 17:56:16 +0800

    Fix login image

--- frontend/components/LoginPage/LoginPage.tsx
+++ frontend/components/LoginPage/LoginPage.tsx
@@ -30,9 +30,10 @@ const Login = () => {
                 <div className="flex xl:justify-center lg:justify-between justify-center items-center flex-wrap h-full g-6">
                     <div className="grow-0 shrink-1 md:shrink-0 basis-auto xl:w-6/12 lg:w-6/12 md:w-9/12 mb-12 md:mb-0">
                         <Image
-                            src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
-                            className="w-full"
-                            alt="Sample image"
+                            src="/images/login-background.webp"
+                            width="700px"
+                            height="500px"
+                            alt="login-background"
                         />
                     </div>
                     <div className="xl:ml-20 xl:w-5/12 lg:w-5/12 md:w-8/12 mb-12 md:mb-0">
