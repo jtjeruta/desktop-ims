d3997be0fce219fb7b20cf17abb027fe59cbf957
dev-tristan <jt.jeruta@gmail.com>
2022-06-21 08:24:54 +0800

    Redirect from index

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,4 +1,21 @@
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+import { useAuthContext } from '../contexts/AuthContext/AuthContext'
+
 const Home = () => {
+    const AuthContext = useAuthContext()
+    const router = useRouter()
+
+    useEffect(() => {
+        if (!AuthContext.user) {
+            router.replace('/login')
+        } else if (AuthContext.user.role === 'employee') {
+            router.replace('/login')
+        } else {
+            router.replace('/users')
+        }
+    }, [AuthContext.user, router])
+
     return <></>
 }
 
