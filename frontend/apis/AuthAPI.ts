693df4ead086979e1e72ac21d04246f7e56f9b45
dev-tristan <jt.jeruta@gmail.com>
2022-06-23 21:27:37 +0800

    Fix axios to get latest token from cookies

--- frontend/apis/AuthAPI.ts
+++ frontend/apis/AuthAPI.ts
@@ -3,7 +3,8 @@ import { User } from '../contexts/AuthContext/types'
 import { AxiosResponse } from 'axios'
 
 export const login = (email: string, password: string) =>
-    Axios.post('/api/v1/auth/login', { email, password })
+    Axios()
+        .post('/api/v1/auth/login', { email, password })
         .then((response): [true, { user: User; token: string }] => [
             true,
             response.data,
@@ -11,6 +12,7 @@ export const login = (email: string, password: string) =>
         .catch((err): [false, AxiosResponse] => [false, err.response])
 
 export const verifyToken = (token: string) =>
-    Axios.post('/api/v1/auth/verify-token', { token })
+    Axios()
+        .post('/api/v1/auth/verify-token', { token })
         .then((response): [true, User] => [true, response.data.user])
         .catch((err): [false, string] => [false, err.response.data])
