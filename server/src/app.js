f02ce06f41aec5d232d88227d8dae0f80f7f3de6
Tristan <jt.jeruta@gmail.com>
2022-09-12 18:39:41 +0800

    Add simulated latency on dev mode

--- server/src/app.js
+++ server/src/app.js
@@ -2,10 +2,12 @@ const express = require('express')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 const cors = require('cors')
+const simulateLatency = require('express-simulate-latency')
 
 const Routes = require('./routes')
 const { dbConnect } = require('./lib/db')
 
+const lag = simulateLatency({ min: 1000, max: 1000 })
 const app = express()
 app.db = dbConnect()
 
@@ -14,6 +16,10 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(lag)
+}
 
 app.use('/', Routes)
 
