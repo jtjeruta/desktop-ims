8d4d086e2bcd733077ae16b235360daa8d93f8a8
Tristan <jt.jeruta@gmail.com>
2022-11-27 19:37:27 +0800

    Remove inventory skeleton

--- frontend/components/AddEditProductForm/AddEditProductForm.tsx
+++ frontend/components/AddEditProductForm/AddEditProductForm.tsx
@@ -7,7 +7,6 @@ import { useAppContext } from '../../contexts/AppContext/AppContext'
 import TextField from '../TextField/TextField'
 import Button from '../Button/Button'
 import { useProductContext } from '../../contexts/ProductContext/ProductContext'
-import AddEditProductFormSkeleton from './Skeleton'
 
 type Props = {
     type?: 'create' | 'update'
@@ -75,43 +74,44 @@ const AddEditProductForm: FC<Props> = (props) => {
         methods.setValue('storeQty', +(ProductContext.product?.storeQty || 0))
     }, [ProductContext.product, methods])
 
-    return isDisabled ? (
-        <AddEditProductFormSkeleton />
-    ) : (
+    return (
         <FormProvider {...methods}>
             <form onSubmit={methods.handleSubmit(onSubmit)}>
                 <div
                     className={clsx(
                         'grid gap-3 mb-2',
-                        !ProductContext.product && ' lg:grid-cols-2'
+                        props.type !== 'update' && ' lg:grid-cols-2'
                     )}
                 >
                     <TextField
                         name="name"
                         label="Name"
                         placeholder="Product 1"
+                        disabled={isDisabled}
                         required
                     />
-                    {!ProductContext.product && (
+                    {props.type !== 'update' && (
                         <TextField
                             name="price"
                             type="number"
                             label="Price"
                             placeholder="100.00"
                             required
+                            disabled={isDisabled}
                             min={0}
                         />
                     )}
                 </div>
                 <div className="grid gap-3 mb-2 lg:grid-cols-2">
-                    {ProductContext.product ? (
+                    {props.type === 'update' ? (
                         <TextField
                             name="price"
                             type="number"
                             label="Price"
                             placeholder="100.00"
-                            required
                             min={0}
+                            disabled={isDisabled}
+                            required
                         />
                     ) : (
                         <TextField
@@ -119,14 +119,16 @@ const AddEditProductForm: FC<Props> = (props) => {
                             type="number"
                             label="Store Quantity"
                             placeholder="100"
-                            required
                             min={0}
+                            disabled={isDisabled}
+                            required
                         />
                     )}
                     <TextField
                         name="company"
                         label="Company"
                         placeholder="Company 1"
+                        disabled={isDisabled}
                     />
                 </div>
                 <div className="grid gap-3 mb-2 lg:grid-cols-2">
@@ -134,15 +136,18 @@ const AddEditProductForm: FC<Props> = (props) => {
                         name="category"
                         label="Category"
                         placeholder="category 1"
+                        disabled={isDisabled}
                     />
                     <TextField
                         name="subCategory"
                         label="Sub Category"
                         placeholder="sub category 1"
+                        disabled={isDisabled}
                     />
                 </div>
                 <div className="flex justify-end">
                     <Button
+                        disabled={isDisabled}
                         loading={
                             AppContext.isLoading('update-product') ||
                             AppContext.isLoading('add-product')
