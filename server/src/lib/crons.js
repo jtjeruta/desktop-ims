f5753475d311706714913e619d4fd54aa0f60343
tristan <jt.jeruta@gmail.com>
2023-05-10 00:49:03 +0800

    Update cron

--- server/src/lib/crons.js
+++ server/src/lib/crons.js
@@ -2,5 +2,6 @@ const startBackup = require('./backup-action')
 const cron = require('node-cron')
 
 module.exports = () => {
-    cron.schedule('0 12 * * *', startBackup)
+    // schedule a weekly backup at 12:00 PM on Wednesday
+    cron.schedule('0 12 * * 3', startBackup)
 }
