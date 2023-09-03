e8f4c941c59f4480c031a5f826ba16ef544ebc6a
tristan <jt.jeruta@gmail.com>
2023-09-03 19:02:23 +0800

    Database backup cron

--- server/src/lib/crons.js
+++ server/src/lib/crons.js
@@ -1,3 +1,11 @@
 const cron = require('node-cron')
+const { backup } = require('../modules/backup-module')
 
-module.exports = () => {}
+module.exports = () => {
+    // Backup database everyday at 12:00 PM
+    cron.schedule('0 12 * * *', async () => {
+        console.info('Backing up database')
+        await backup()
+        console.info('Database backup completed')
+    })
+}
