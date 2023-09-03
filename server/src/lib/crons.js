d6570eb3bd7027f85901bd823486419f7c2c3e98
tristan <jt.jeruta@gmail.com>
2023-09-03 19:11:19 +0800

    Run backup every 2 hours instead

--- server/src/lib/crons.js
+++ server/src/lib/crons.js
@@ -2,8 +2,8 @@ const cron = require('node-cron')
 const { backup } = require('../modules/backup-module')
 
 module.exports = () => {
-    // Backup database everyday at 12:00 PM
-    cron.schedule('0 12 * * *', async () => {
+    // Backup database every 2 hours
+    cron.schedule('0 */2 * * *', async () => {
         console.info('Backing up database')
         await backup()
         console.info('Database backup completed')
